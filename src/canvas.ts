1ed67fdae5f30768d4beb1ec900b7318368cb246
jon-edward <39141152+jon-edward@users.noreply.github.com>
2022-11-26 13:17:22 -0500

    Create canvas.ts

---
+++ src/canvas.ts
@@ -0,0 +1,14 @@
+import BaseCanvas from "./baseCanvas";
+
+export default class Canvas extends BaseCanvas {
+    
+    setup() {
+        this.p5.createCanvas(100, 100)
+        this.p5.fill(0)
+    }
+    
+    draw() {
+        this.p5.background(255)
+        this.p5.circle(50, 50, 10)
+    }
+}
